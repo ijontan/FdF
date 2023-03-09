a46967b40107f202cc01968240da4e73c6fdb61e
ijontan <ijontan9@gmail.com>
2023-03-10 02:02:39 +0800

    made projection to round to the correct pixel

--- src/projection/orthographic.c
+++ src/projection/orthographic.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 15:08:06 by itan              #+#    #+#             */
-/*   Updated: 2023/03/09 14:39:43 by itan             ###   ########.fr       */
+/*   Updated: 2023/03/10 00:55:12 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_offset	orthographic_projection(double v[3])
 {
 	t_offset	offset;
 
-	offset.x = (int)v[0];
-	offset.y = (int)v[1];
+	offset.x = (int)(v[0] + 0.5);
+	offset.y = (int)(v[1] + 0.5);
 	return (offset);
 }
--- src/projection/perspective.c
+++ src/projection/perspective.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 12:34:44 by itan              #+#    #+#             */
-/*   Updated: 2023/03/09 14:50:09 by itan             ###   ########.fr       */
+/*   Updated: 2023/03/10 00:55:46 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_offset	perspective_projection(double v[3], double focal, double screen_p)
 	double		dis_from_focal;
 
 	dis_from_focal = screen_p - v[2] + focal;
-	offset.x = v[0] / dis_from_focal * focal;
-	offset.y = v[1] / dis_from_focal * focal;
+	offset.x = (v[0] / dis_from_focal * focal) + 0.5;
+	offset.y = (v[1] / dis_from_focal * focal) + 0.5;
 	return (offset);
 }
